eeaae65201ecacbf0a05e02949526f7853a74f5e
RedHotUnicorn <zhitnik@vdi-zhitnik.msk.miit.ru>
2015-11-09 21:03:24 +0300

    Add access to Iterator. Correted input new mess on Forum

--- RequestViewController/src/ru/miit/my/cabinet/request/view/ViewRequest.java
+++ RequestViewController/src/ru/miit/my/cabinet/request/view/ViewRequest.java
@@ -1,3 +1,4 @@
+
 package ru.miit.my.cabinet.request.view;
 
 import oracle.adf.model.BindingContext;
@@ -8,6 +9,18 @@ import oracle.binding.OperationBinding;
 
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
+
+import javax.faces.event.ActionEvent;
+
+import oracle.adf.model.binding.DCBindingContainer;
+import oracle.adf.model.binding.DCIteratorBinding;
+
+
+
+import oracle.jbo.Row;
+import oracle.jbo.ViewObject;
+
+import ru.miit.my.cabinet.request.view.utils.ADFUtils;
 
 public class ViewRequest {
     public ViewRequest() {
@@ -79,7 +92,52 @@ public class ViewRequest {
     public void checked(FacesContext facesContext, UIComponent uIComponent, Object object) {
         this.setCheckbox(!this.isCheckbox());
         System.out.println(this.isCheckbox());
+        
     }
+
+
+    public void sbc2_validator(FacesContext facesContext, UIComponent uIComponent, Object object) {
+       // setAdd(true);
+    }
+    
+    public DCBindingContainer getDCBindingsContainer() {
+        DCBindingContainer bindingsContainer =
+            (DCBindingContainer)BindingContext.getCurrent().getCurrentBindingsEntry();
+        
+        return bindingsContainer;
+    }
+
+
+    public String read() {
+        /*try{  DCBindingContainer bindings = this.getDCBindingsContainer();
+        DCIteratorBinding itorBinding =
+            bindings.findIteratorBinding("Typetest1View_POCHTA1Iterator");
+            System.out.println("Step1");
+        ViewObject vo = itorBinding.getViewObject();
+            System.out.println("Step2");
+        Row[]  selectedRolesRows = vo.getFilteredRows("YesNo",1);//vo.getFilteredRows("YesNo", true);
+           
+        System.out.println(selectedRolesRows.length);
+        for(Row row : selectedRolesRows){
+            System.out.println("Text: "+row.getAttribute("Text"));
+            
+        }
+        }catch(Exception e){
+                System.out.println("fuck");
+                
+            }
+        return null;*/
+        
+        Row row = ADFUtils.findIterator("RequestforummessageView3Iterator").getCurrentRow();
+      //  System.out.println(row.getAttribute("Text")+" "+row.getAttribute("YesNo")+" "+row.getKey());
+        
+        Row[] rows = ADFUtils.findIterator("RequestforummessageView3Iterator").getAllRowsInRange();
+        for(int i=0;i<rows.length;i++) {
+            System.out.println(rows[i].getAttribute("Text")+" "+rows[i].getAttribute("YesNo"));
+        }
+        return null;
+    }
+
 
     
 }
---
+++ RequestViewController/src/ru/miit/my/cabinet/request/view/utils/package-info.java
@@ -0,0 +1,2 @@
+package ru.miit.my.cabinet.request.view.utils;
+
