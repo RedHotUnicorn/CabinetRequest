7e41d8c62b0233d15e20951423812410f2246de5
RedHotUnicorn <zhitnik@vdi-zhitnik.msk.miit.ru>
2015-11-12 20:34:46 +0300

    Allright! I Add creation date and now checkbox render by clicking button!

--- RequestViewController/src/ru/miit/my/cabinet/request/view/ViewRequest.java
+++ RequestViewController/src/ru/miit/my/cabinet/request/view/ViewRequest.java
@@ -1,37 +1,40 @@
 
 package ru.miit.my.cabinet.request.view;
 
+
+import java.text.SimpleDateFormat;
+
+import java.util.Calendar;
+
 import oracle.adf.model.BindingContext;
 
 import oracle.binding.BindingContainer;
 import oracle.binding.OperationBinding;
 
-
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 
-import javax.faces.event.ActionEvent;
-
 import oracle.adf.model.binding.DCBindingContainer;
-import oracle.adf.model.binding.DCIteratorBinding;
-
-
 
 import oracle.jbo.Row;
-import oracle.jbo.ViewObject;
 
 import ru.miit.my.cabinet.request.view.utils.ADFUtils;
 
 public class ViewRequest {
     public ViewRequest() {
         super();
-        checkbox=false;
-        System.out.println("FUCKIN BEAN!!!");
+        checkbox = false;
+
+        System.out.println("-------->BEAN CREATED!!!");
     }
-    private boolean checkbox ;
-    
+
+    private boolean checkbox;
+    static private boolean enableDeleteCheckbox = false; //Нажали на кнопочку Редактировать
     private String mes;
-    static private boolean add = false;
+    public java.sql.Date dateNow;
+    public String dateNow2;
+    static private boolean add = false; //Что то изменили на странице кнопка Сохранить активировалась
+
     public void setMes(String mes) {
         this.mes = mes;
     }
@@ -45,16 +48,31 @@ public class ViewRequest {
     }
 
     public String cb2_action() {
+
+        Calendar currentDate = Calendar.getInstance(); //Get the current date
+        SimpleDateFormat formatter =
+            new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); //format it as per your requirement HH:mm:ss
+        dateNow =
+                new java.sql.Date(currentDate.getTime().getTime()); //new java.sql.Date(System.currentTimeMillis()); //new java.sql.Date(currentDate.getTime().getTime());
+        System.out.println(formatter.format(dateNow));
+        dateNow2 = formatter.format(dateNow);
+        System.out.println(dateNow2);
+
+
         BindingContainer bindings = getBindings();
+
         OperationBinding operationBinding = bindings.getOperationBinding("ForumCreateInsert");
         Object result = operationBinding.execute();
+
         if (!operationBinding.getErrors().isEmpty()) {
             return null;
         }
+
         this.setMes("");
-        System.out.println("cb2Bool "+add);
+        System.out.println("cb2Bool " + add);
         setAdd(true);
-        System.out.println("cb2Bool "+add);
+        System.out.println("cb2Bool " + add);
+
         return null;
     }
 
@@ -67,17 +85,17 @@ public class ViewRequest {
     }
 
     public String cb3_action() {
-        System.out.println("cb3Bool "+add);
+        System.out.println("cb3Bool " + add);
         setAdd(false);
-        System.out.println("cb3Bool "+add);
+        System.out.println("cb3Bool " + add);
         return "Commit";
     }
 
 
     public String cb1_action() {
-        System.out.println("cb1Bool "+add);
+        System.out.println("cb1Bool " + add);
         setAdd(false);
-        System.out.println("cb1Bool "+add);
+        System.out.println("cb1Bool " + add);
         return "RollBack";
     }
 
@@ -92,54 +110,64 @@ public class ViewRequest {
     public void checked(FacesContext facesContext, UIComponent uIComponent, Object object) {
         this.setCheckbox(!this.isCheckbox());
         System.out.println(this.isCheckbox());
-        
+
     }
 
 
     public void sbc2_validator(FacesContext facesContext, UIComponent uIComponent, Object object) {
-       // setAdd(true);
+        // setAdd(true);
     }
-    
+
     public DCBindingContainer getDCBindingsContainer() {
         DCBindingContainer bindingsContainer =
             (DCBindingContainer)BindingContext.getCurrent().getCurrentBindingsEntry();
-        
+
         return bindingsContainer;
     }
 
 
     public String read() {
-        /*try{  DCBindingContainer bindings = this.getDCBindingsContainer();
-        DCIteratorBinding itorBinding =
-            bindings.findIteratorBinding("Typetest1View_POCHTA1Iterator");
-            System.out.println("Step1");
-        ViewObject vo = itorBinding.getViewObject();
-            System.out.println("Step2");
-        Row[]  selectedRolesRows = vo.getFilteredRows("YesNo",1);//vo.getFilteredRows("YesNo", true);
-           
-        System.out.println(selectedRolesRows.length);
-        for(Row row : selectedRolesRows){
-            System.out.println("Text: "+row.getAttribute("Text"));
-            
-        }
-        }catch(Exception e){
-                System.out.println("fuck");
-                
-            }
-        return null;*/
-        
-        Row row = ADFUtils.findIterator("RequestforummessageView3Iterator").getCurrentRow();
-      //  System.out.println(row.getAttribute("Text")+" "+row.getAttribute("YesNo")+" "+row.getKey());
-        
+        //Row row = ADFUtils.findIterator("RequestforummessageView3Iterator").getCurrentRow();
         Row[] rows = ADFUtils.findIterator("RequestforummessageView3Iterator").getAllRowsInRange();
-        for(int i=0;i<rows.length;i++) {
-            if(rows[i].getAttribute("YesNo").equals(true))
+        for (int i = 0; i < rows.length; i++) {
+            if (rows[i].getAttribute("YesNo").equals(true))
                 rows[i].remove();
-            //System.out.println(rows[i].getAttribute("Text")+" "+rows[i].getAttribute("YesNo"));
+
         }
         return null;
     }
 
 
-    
+    public void cb5_action() {
+        System.out.println("checbox was " + enableDeleteCheckbox);
+        setEnableDeleteCheckbox(!isEnableDeleteCheckbox());
+        System.out.println("now checbox is " + enableDeleteCheckbox);
+    }
+
+    public void setEnableDeleteCheckbox(boolean enableDeleteCheckbox) {
+        //System.out.println("checbox was "+enableDeleteCheckbox);
+        this.enableDeleteCheckbox = enableDeleteCheckbox;
+        // System.out.println("now checbox is "+enableDeleteCheckbox);
+
+    }
+
+    public boolean isEnableDeleteCheckbox() {
+        return enableDeleteCheckbox;
+    }
+
+    public void setDateNow(java.sql.Date dateNow) {
+        this.dateNow = dateNow;
+    }
+
+    public java.sql.Date getDateNow() {
+        return dateNow;
+    }
+
+    public void setDateNow2(String dateNow2) {
+        this.dateNow2 = dateNow2;
+    }
+
+    public String getDateNow2() {
+        return dateNow2;
+    }
 }
